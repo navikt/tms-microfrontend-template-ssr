e762a79239a43d19c2dedd43acede2641165b68d
Nima Hakimi <nima.hakimi2@nav.no>
2025-04-25 15:19:20 +0200

    Add example fetch for displaying dynamic content

--- src/language/text.ts
+++ src/language/text.ts
@@ -1,12 +1,12 @@
 export const text = {
   title: {
-    nb: "Norsk bokmål tms-astro-template",
-    en: "English tms-astro-template",
-    nn: "Norsk nynorsk tms-astro-template",
+    nb: 'Norsk bokmål tms-astro-template',
+    en: 'English tms-astro-template',
+    nn: 'Norsk nynorsk tms-astro-template',
   },
   name: {
-    nb: "Navn",
-    en: "Name",
-    nn: "Navn",
+    nb: 'Navn',
+    en: 'Name',
+    nn: 'Navn',
   },
 };
