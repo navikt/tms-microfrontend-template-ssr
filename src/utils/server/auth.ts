4f6179087f97eb7c9be4be2b477402b548c4dfbb
sasoria <sergio.alberto.arevalo.soria@nav.no>
2025-05-08 11:11:15 +0200

    Refactor isLocal

--- src/utils/server/auth.ts
+++ src/utils/server/auth.ts
@@ -1,4 +1,4 @@
 import type { APIContext } from "astro";
 
 export const isInternal = (context: APIContext) => context.request.url.includes("/internal");
-export const isLocal = process.env.NODE_ENV === 'development';
+export const isLocal = process.env.NODE_ENV === "development";
