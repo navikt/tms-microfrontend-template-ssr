cf197244c07bc54417b4fb31e29d3cd06f75f928
Nima Hakimi <nima.hakimi2@nav.no>
2025-05-12 11:28:23 +0200

    Remove base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import prefixer from "postcss-prefix-selector";
 
 // https://astro.build/config
 export default defineConfig({
-  base: "/tms-microfrontend-template-ssr/",
   build: {
     assetsPrefix: "https://cdn.nav.no/min-side/tms-microfrontend-test",
   },
