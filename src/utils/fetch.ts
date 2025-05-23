bb8aa2672bd9165cf883ebb0620091b0713f398c
sasoria <sergio.alberto.arevalo.soria@nav.no>
2025-05-23 18:54:03 +0200

    Refactor fetch

---
+++ src/utils/fetch.ts
@@ -0,0 +1,16 @@
+
+export const fetchExampleData = async (oboToken: string, url: string) => {
+    const response = await fetch(url, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${oboToken}`,
+        },
+    });
+
+    if (!response.ok) {
+        throw new Error(`Http error with status: ${response.status}`);
+    }
+
+    return await response.json();
+};
