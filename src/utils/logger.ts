0235ad7404950e83e88a2abefc6cbd8c92057005
Nima <nima.hakimi@nav.no>
2025-08-18 16:12:29 +0200

    move logger logic from test repo

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,5 +1,24 @@
-import pino from "pino-http";
+import { logs, NodeSDK, tracing } from "@opentelemetry/sdk-node";
+import pino from "pino";
 
-const logger = pino({}).logger;
+const sdk = new NodeSDK({
+  spanProcessor: new tracing.SimpleSpanProcessor(new tracing.ConsoleSpanExporter()),
+  logRecordProcessor: new logs.SimpleLogRecordProcessor(new logs.ConsoleLogRecordExporter()),
+});
 
-export default logger;
+sdk.start();
+
+process.on("SIGTERM", () => {
+  sdk
+    .shutdown()
+    .then(() => logger.info("Opentelemetry Tracing terminated"))
+    .catch((error) => logger.error("Error terminating Opentelemetry Tracing", error))
+    .finally(() => process.exit(0));
+});
+
+export const logger = pino({
+  timestamp: () => `,"@timestamp":"${new Date().toISOString()}"`,
+  formatters: {
+    level: (label) => ({ level: label.toUpperCase() }),
+  },
+});
