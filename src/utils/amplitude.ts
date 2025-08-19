7c8b4c0dd6f47109b1954a63d7fbb1c77d8866cf
Nima <nima.hakimi@nav.no>
2025-08-19 11:06:24 +0200

    remove unused file

--- src/utils/amplitude.ts
+++
@@ -1,9 +0,0 @@
-import { getAmplitudeInstance } from "@navikt/nav-dekoratoren-moduler";
-
-const logger = getAmplitudeInstance("dekoratoren");
-
-export const logEvent = (data: string, kategori: string, lenketekst: string) => {
-  logger("navigere", { komponent: data, kategori: kategori, lenketekst: lenketekst }).catch(() =>
-    console.warn("Uninitialized amplitude"),
-  );
-};
