5fc7e0d2720b6599b7a5d569c934bcd3c304d2a6
sasoria <sergio.alberto.arevalo.soria@nav.no>
2025-08-21 12:35:51 +0200

    Refactor analytics

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -1,10 +1,10 @@
 import { getAnalyticsInstance } from "@navikt/nav-dekoratoren-moduler";
 import type { AmplitudeEvent } from "@navikt/nav-dekoratoren-moduler";
 
-type ExtendedAmpltitudeEvent = AmplitudeEvent<'navigere', { lenketekst: string }>;
+type ExtendedAmpltitudeEvent = AmplitudeEvent<"navigere", { lenketekst: string }>;
 
 const analyticsLogger = getAnalyticsInstance<ExtendedAmpltitudeEvent>("tms-microfrontend-template-ssr");
 
-export const logEvent = () => {
-    analyticsLogger("navigere", { lenketekst: "tekst" });
+export const logEvent = async (lenketekst: string) => {
+  await analyticsLogger("navigere", { lenketekst: lenketekst });
 };
