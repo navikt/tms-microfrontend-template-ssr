f33d5c745f6c9f1f4b8092e09e9f4011612d0277
sasoria <sergio.alberto.arevalo.soria@nav.no>
2025-11-24 15:55:18 +0100

    Sett opp mock

--- mock/server.ts
+++ mock/server.ts
@@ -1,21 +1,28 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
-import example from "./data/example.json";
+import { motebehovUtenSvar } from "./fixtures/dialogmote/motebehovUtenSvar.ts";
+import { createAvlysningsBrev, createInnkallingsBrev } from "./fixtures/factories/brev.ts";
+import { createReferatBrev, createReferatEndretBrev } from "./fixtures/factories/brev.ts";
+import { leggTilDagerPaDato } from "@src/utils/dateUtils.ts";
 
 const api = new Hono();
 
-// Enable CORS for all routes
-api.use(
-  "/*",
-  cors({
-    origin: "http://localhost:4321",
-    credentials: true,
-  }),
-);
+api.use("/*", cors({ origin: "http://localhost:4321", credentials: true }));
 
-api.get("/api/tms-astro-template", (c) => {
-  return c.json(example);
+api.get("/api/dialogmote", (c) => {
+  return c.json([
+      createInnkallingsBrev({ createdAt: leggTilDagerPaDato(new Date(), -10).toISOString() }),
+      createAvlysningsBrev({ createdAt: leggTilDagerPaDato(new Date(), -6).toISOString() }),
+      createInnkallingsBrev({ createdAt: leggTilDagerPaDato(new Date(), -5).toISOString() }),
+      createReferatBrev({ createdAt: leggTilDagerPaDato(new Date(), -4).toISOString() }),
+      createInnkallingsBrev({ createdAt: leggTilDagerPaDato(new Date(), -3).toISOString() }),
+      createReferatEndretBrev({ createdAt: leggTilDagerPaDato(new Date(), -103).toISOString() }),
+  ]);
+});
+
+api.get("/api/motebehov", (c) => {
+    return c.json(motebehovUtenSvar);
 });
 
 serve(api);
