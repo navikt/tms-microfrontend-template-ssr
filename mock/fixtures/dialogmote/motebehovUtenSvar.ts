162a5f60da03aae1832833ee6c482fe25ba3244e
sasoria <sergio.alberto.arevalo.soria@nav.no>
2026-04-29 10:12:23 +0200

    Revert "Sett opp mock"
    This reverts commit f33d5c745f6c9f1f4b8092e09e9f4011612d0277.

--- mock/fixtures/dialogmote/motebehovUtenSvar.ts
+++
@@ -1,7 +0,0 @@
-import type { MotebehovStatusDTO } from "../../../schema/motebehovSchema.ts";
-
-export const motebehovUtenSvar: MotebehovStatusDTO = {
-    visMotebehov: true,
-    skjemaType: "SVAR_BEHOV",
-    motebehov: null,
-};
